e91aae067080267b05b8f03949cf91d8ceb6e736
Krishna Rawat <krishna_rawat@persistent.com>
2022-08-03 14:43:02 +0530

    Communication is established between permission and employee API

--- employee-info/src/main/java/com/psl/employeeinfo/controller/EmployeeController.java
+++ employee-info/src/main/java/com/psl/employeeinfo/controller/EmployeeController.java
@@ -3,10 +3,14 @@ package com.psl.employeeinfo.controller;
 import com.psl.employeeinfo.model.Employee;
 import com.psl.employeeinfo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -14,8 +18,19 @@ import java.util.NoSuchElementException;
 @RestController
 public class EmployeeController {
 
+    @Bean
+    public RestTemplate getRestTamplate(){
+        return new RestTemplate();
+    }
+
+    @Lazy
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Autowired
     private EmployeeService employeeService;
+
+    static final String Permission_URL = "http://localhost:8084/";
 
     @GetMapping("/employee")
     private List<Employee> getAllEmployee(){
@@ -33,62 +48,77 @@ public class EmployeeController {
 
     @PostMapping("/employee")
     private Object saveEmployee(@RequestBody Employee emp){
-        try{
-            if(emp.getEmp_id().startsWith("PSL")){
-                if(employeeService.getEmployeeById(emp.getEmp_id()).getEmp_id() == emp.getEmp_id()) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body("Data with Id "+emp.getEmp_id()+" already in database!");
-                }else{
-                    employeeService.saveEmployee(emp);
+        if(restTemplate.exchange(Permission_URL+"permission/EmployeeApp1", HttpMethod.GET, null, Boolean.class).getBody()) {
+            try {
+                if (emp.getEmp_id().startsWith("PSL")) {
+                    if (employeeService.getEmployeeById(emp.getEmp_id()).getEmp_id() == emp.getEmp_id()) {
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body("Data with Id " + emp.getEmp_id() + " already in database!");
+                    } else {
+                        employeeService.saveEmployee(emp);
+                    }
+                } else {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Employee Id - The format of Id should be like PSL101");
                 }
-            }else{
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Employee Id - The format of Id should be like PSL101");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Employee Record is not Saved");
             }
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Employee Record is not Saved");
+            return ResponseEntity.status(HttpStatus.CREATED).body("Employee " + emp.getEmp_id() + " details added successfully");
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You do not have permission to create employee");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body("Employee "+emp.getEmp_id()+" details added successfully");
     }
 
     @PutMapping("/employee")
     private Object updateEmployee(@RequestBody Employee emp){
-        Employee empRec;
-        try{
-            empRec = employeeService.getEmployeeById(emp.getEmp_id());
-            if(empRec.getEmp_id().equals(emp.getEmp_id())) {
-                if ((empRec.getEmp_name().equals(emp.getEmp_name())) &&
-                        (empRec.getEmp_profile().equals(emp.getEmp_profile())) &&
-                        (empRec.getEmp_salary() == emp.getEmp_salary())) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body("No Change in the Data!");
+        if(restTemplate.exchange(Permission_URL+"permission/EmployeeApp1", HttpMethod.GET, null, Boolean.class).getBody()) {
+            Employee empRec;
+            try {
+                empRec = employeeService.getEmployeeById(emp.getEmp_id());
+                if (empRec.getEmp_id().equals(emp.getEmp_id())) {
+                    if ((empRec.getEmp_name().equals(emp.getEmp_name())) &&
+                            (empRec.getEmp_profile().equals(emp.getEmp_profile())) &&
+                            (empRec.getEmp_salary() == emp.getEmp_salary())) {
+                        return ResponseEntity.status(HttpStatus.CONFLICT).body("No Change in the Data!");
+                    } else {
+                        employeeService.updateEmployee(emp);
+                        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Resource updated Successfully!");
+                    }
                 } else {
-                    employeeService.updateEmployee(emp);
-                    return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Resource updated Successfully!");
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No such Employee Id present in database");
                 }
-            }else{
+            } catch (NoSuchElementException e) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No such Employee Id present in database");
             }
-        }catch(NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No such Employee Id present in database");
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You do not have permission to update employee");
         }
     }
 
     @DeleteMapping("/employee/{emp_id}")
     private Object deleteEmployeeById(@PathVariable("emp_id") String emp_id){
-        try{
-            employeeService.deleteEmployeeById(emp_id);
-            return ResponseEntity.status(HttpStatus.OK).body("Employee Id "+emp_id+" Deleted Successfully!");
-        }catch(EmptyResultDataAccessException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee Id Not In Database!");
+        if(restTemplate.exchange(Permission_URL+"permission/EmployeeApp1", HttpMethod.GET, null, Boolean.class).getBody()) {
+            try {
+                employeeService.deleteEmployeeById(emp_id);
+                return ResponseEntity.status(HttpStatus.OK).body("Employee Id " + emp_id + " Deleted Successfully!");
+            } catch (EmptyResultDataAccessException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee Id Not In Database!");
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You do not have permission to delete employee");
         }
-
     }
 
     @DeleteMapping("/employee")
     private Object deleteAllEmployee(){
-        try{
-            employeeService.deleteAllEmployee();
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Employees are Deleted Successfully!");
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.GONE).body("Unable to delete Data");
+        if(restTemplate.exchange(Permission_URL+"permission/EmployeeApp1", HttpMethod.GET, null, Boolean.class).getBody()) {
+            try {
+                employeeService.deleteAllEmployee();
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Employees are Deleted Successfully!");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.GONE).body("Unable to delete Data");
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You do not have permission to delete employee");
         }
     }
 }
